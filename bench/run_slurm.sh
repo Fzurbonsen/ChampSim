aa966d2af974090029840b1e426b478a29cc7b63
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2025-11-22 19:34:34 +0100

    [warmup]: bug fix

--- bench/run_slurm.sh
+++ bench/run_slurm.sh
@@ -16,4 +16,4 @@ podman load -i $CHAMPSIM/champsim_latest.tar
 podman run --rm \
     -v $CHAMPSIM:/ChampSim \
     $IMAGE \
-    /bin/bash -c " cd ./bench && \ python3 run_sim.py --file $CONFIG"
+    python3 run_sim.py --file $CONFIG
