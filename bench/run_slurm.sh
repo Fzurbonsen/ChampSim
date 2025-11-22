9899b5e0d6e87ec1d908e90a7a5827a8692167f8
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2025-11-22 19:03:14 +0100

    [warmup]: image ready to build

--- bench/run_slurm.sh
+++ bench/run_slurm.sh
@@ -3,8 +3,15 @@
 #SBATCH --array=0-13
 #SBATCH --output=logs/out_%A_%a.log
 
+PWD_DIR=$(pwd)
+IMAGE=champsim:latest
+CHAMPSIM=$PWD_DIR/../
+
 module load python/3.12
 
 CONFIG=params/config_${SLURM_ARRAY_TASK_ID}.json
 
-python3 run_sim.py --file $CONFIG
+podman run --rm \
+    -v $CHAMPSIM:/ChampSim \
+    $IMAGE \
+    python3 run_sim.py --file $CONFIG
--- podman_build.sh
+++ podman_build.sh
@@ -3,5 +3,7 @@
 export http_proxy="http://proxy.ethz.ch:3128"
 export https_proxy="https://proxy.ethz.ch:3128"
 
+rm -rf champsim_latest.tar
 podman rmi champsim:latest
 podman build -t champsim:latest .
+podman save -o champsim_latest.tar champsim:latest
