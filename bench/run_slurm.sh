0e2f7088aecd16979ddba154e1a690a403702194
Frederic zurBonsen <fzurbonsen@safari-proxy>
2025-11-22 17:14:22 +0100

    [warmup]: adapted run_slurm.sh

--- bench/run_slurm.sh
+++ bench/run_slurm.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=sim
 #SBATCH --array=0-13
+#SBATCH --output=logs/out_%A_%a.log
+
+module load python/3.12
 
 CONFIG=params/config${SLURM_ARRAY_TASK_ID}.json
 
-python run_sim.py --file $CONFIG
+python3 run_sim.py --file $CONFIG
