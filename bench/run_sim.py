e09991785faf46f16c80b36f4ab58b22765f62dd
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2025-11-28 23:53:56 +0100

    udpated bench script

--- bench/run_sim.py
+++ bench/run_sim.py
@@ -30,13 +30,13 @@ traces = [
 ]
 out_path = "out/" # this is not relative to the host_path but relative to the script location
 csv_path = "csv/" # this is not relative to the host_path but relative to the script location
-csv_file = "task5_pythia_fullBW.csv"
+csv_file = "task4_delta_currelation_limitBW_test.csv"
 warmup_instr = int(1e7) # should be 1e7
 sim_instr = int(5e7) # should be 5e7
 config_core = "1C"
 # config_bandwidth = "fullBW"
 config_bandwidth = "fullBW"
-config_prefetcher = "pythia"
+config_prefetcher = "delta_correlation"
 # config_prefetcher = "no"
 
 # global ui state
