89e096559c48ec98e26be1da002dd2e4b19b3400
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2025-11-29 12:38:07 +0100

    base version

--- bench/run_sim.py
+++ bench/run_sim.py
@@ -30,7 +30,7 @@ traces = [
 ]
 out_path = "out/" # this is not relative to the host_path but relative to the script location
 csv_path = "csv/" # this is not relative to the host_path but relative to the script location
-csv_file = "task4_delta_correlation_patched_limitBW.csv"
+csv_file = "task4_delta_correlation_base_limitBW.csv"
 warmup_instr = int(1e7) # should be 1e7
 sim_instr = int(5e7) # should be 5e7
 config_core = "1C"
